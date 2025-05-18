a98471488fb27cf46c887828922748675e8e53b8
sigma-k-365.git <kawamoto.siguma@gmail.com>
2025-05-18 20:55:00 +0200

    Ratio of trump related news

--- scripts/01/group_by_month.r
+++ scripts/01/group_by_month.r
@@ -11,5 +11,9 @@ ggplot(count_by_month, aes(x=rounded_by_month, y=count)) + geom_line() + geom_po
 count_trump_by_month <- df_txt_date_news %>% 
     filter(grepl("Trump", news) ) %>%
     group_by(rounded_by_month = lubridate::floor_date(date, 'month')) %>%
-    summarize(count = n())
-ggplot(count_trump_by_month, aes(x=rounded_by_month, y=count)) + geom_line() + geom_point(color = "red") + scale_x_date(limits = as.Date(c("2024-01-01", "2025-06-01")), date_breaks = "month", date_labels = "%m %Y")
+    summarize(trump_count = n())
+ggplot(count_trump_by_month, aes(x=rounded_by_month, y=trump_count)) + geom_line() + geom_point(color = "red") + scale_x_date(limits = as.Date(c("2024-01-01", "2025-06-01")), date_breaks = "month", date_labels = "%m %Y")
+
+# join two tables
+join <- join(x=count_by_month, y=count_trump_by_month, by = "rounded_by_month") %>% mutate(trump_ratio= trump_count/count ) 
+ggplot(join, aes(x=rounded_by_month, y=trump_ratio)) + geom_line() + geom_point(color = "red") + scale_x_date(limits = as.Date(c("2024-01-01", "2025-06-01")), date_breaks = "month", date_labels = "%m %Y")
